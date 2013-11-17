2deae82e87dedab18eafbc56c3fe6855b776390a
bms@barwap.com <bms@barwap.com>
2013-11-17 21:46:20 +0000

    Command to compres to one stl file

--- cage/make_rows.sh
+++ cage/make_rows.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+mkdir rows
+rm rows/*
 for i in {0..15}
 do
    python cage_im.py -s test.scad  -x 15 -y 15 -j $i ; openscad -o rows/row$i.stl test.scad
    echo "Welcome $i times"
 done
+
+python cage_im.py -s test.scad  -x 15 -y 15 -j $i -n -m ; openscad -o rows/frame.stl test.scad
+
+# Use meshlabserver to cobin into one stl file
+meshlabserver -i ./rows/* -o cage_mesh_cleaned.stl -s mesh_flatten.mlx
